73fff1fb592702e074bd8b7ed9d813ed5460921f
MbusyaNgesa <theengesa@gmail.com>
2025-03-05 14:14:57 +0300

    Returning data from AI in json format

--- server/server.js
+++ server/server.js
@@ -5,7 +5,16 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "What are the ingredients for ugali";
+// const prompt = "What are the ingredients for ugali";
+
+const prompt = `List two popular biriani recipes using this JSON schema:
+
+Recipe = {'recipeName': string}
+Ingredients = {'recipeIngredients': string}
+Instructions = {'recipeInstructions': string}
+Return: Array<Recipe>
+Return: Array<Ingredients>
+Return: <Instructions>`;
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
